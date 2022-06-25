1da06504ef0ad4c0f4497fa85d3f4803e9bc54b9
Nabski89 <Nabski@Hotmail.com>
2022-06-24 23:25:46 -0400

    Starter Dialogue, 16 defenders, start of 3 scions

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -8,8 +8,14 @@ public class Spawner : MonoBehaviour
     public int MobCountMax = 3;
     public int SpawnIntervalSeconds = 5;
 
-    public GameObject[] SpawnedMob;
+    public GameObject SpawnedMob;
 
+    SpriteRenderer m_SpriteRenderer;
+    void Start()
+    {
+        m_SpriteRenderer = GetComponent<SpriteRenderer>();
+        m_SpriteRenderer.sprite = SpawnedMob.GetComponent<SpriteRenderer>().sprite;
+    }
 
     void Update()
     {
@@ -20,8 +26,10 @@ public class Spawner : MonoBehaviour
             if (transform.childCount < MobCountMax)
             {
                 cooldown = 0;
-                Debug.Log(SpawnedMob.Length);
-                Instantiate(SpawnedMob[Random.Range(0, SpawnedMob.Length)], transform.position+Vector3.back, Quaternion.identity, transform);
+
+                Instantiate(SpawnedMob, transform.position + Vector3.back, Quaternion.identity, transform);
+                //                Debug.Log(SpawnedMob.Length);
+                //       Instantiate(SpawnedMob[Random.Range(0, SpawnedMob.Length)], transform.position+Vector3.back, Quaternion.identity, transform);
             }
         }
     }
